0fadf0970d7d29d4b6a63eb7b96c6a189901138f
Ken1981m <primdrive1@gmail.com>
2023-04-09 21:01:26 +0200

    Third commit

--- src/components/Inntektregnskap.jsx
+++ src/components/Inntektregnskap.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { fetchData, fetchParamData } from './../utils/dataUtil.jsx';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-
+import ReactDataGrid from 'react-data-grid';
 
 function Inntektregnskap() {
    const [leilighetRows, setLeilighetRows] = useState([]);
@@ -10,7 +10,7 @@ function Inntektregnskap() {
    const [leilighetId, setleilighetId] = useState('');
    const [dato, setDato] = useState(null);
 
-   const [inntektData, setInntektData] = useState([]);
+   const [inntektData, setInntektData] = useState({});
 
     useEffect(() => {
           fetchData('/hentLeiligheter')
@@ -20,7 +20,7 @@ function Inntektregnskap() {
           if (leilighetId != null && dato != null) {
             const aar = dato.getFullYear();
             const data = { leilighetId, aar };
-            fetchParamData('/hentInntekt', data)
+            fetchParamData('/hentInntektRegnskap', data)
                 .then(res => res.json())
                 .then(data => setInntektData(data));
           }
@@ -29,6 +29,12 @@ function Inntektregnskap() {
     const handleLeilighetIdChange = event => {
             setleilighetId(event.target.value);
         };
+
+
+    const columns = [
+      { key: 'label', name: 'Label', minWidth: '50%' },
+      { key: 'belop', name: 'Beløp', minWidth: '50%' },
+    ];
 
     return (
      <>
@@ -48,26 +54,16 @@ function Inntektregnskap() {
                 />
 
         {inntektData.length > 0 ? (
-            <table>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Age</th>
-                  <th>Email</th>
-                </tr>
-              </thead>
-              <tbody>
-                {inntektData.map((row, index) => (
-                  <tr key={index} className={index % 2 === 0 ? 'white-row' : 'grey-row'}>
-                    <td>{row.mnd}</td>
-                    <td>{row.belop}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-        ) : (
-            <p/>
-        )}
+           <div style={{ padding: '20px 0 0 0' }}>
+                <ReactDataGrid className="gridStyle"
+                    columns={columns}
+                    rows={inntektData}
+                    rowsCount={inntektData.length}
+                  />
+            </div>
+         ) : (
+             <p/>
+         )}
 
         </>
       );
