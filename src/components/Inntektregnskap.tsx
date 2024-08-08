d28dc03f209ee45cda484d61bb7a909642be7143
Ken1981m <primdrive1@gmail.com>
2024-08-08 21:58:25 +0200

    Mer logikk på inntektregnskap slik at flere år kan hentes opp

--- src/components/Inntektregnskap.tsx
+++ src/components/Inntektregnskap.tsx
@@ -11,10 +11,31 @@ export function Inntektregnskap() {
 
    const [leilighetId, setleilighetId] = useState('');
    const [dato, setDato] = useState(null);
+   const [tilDato, setTilDato] = useState(null);
 
    const [inntektData, setInntektData] = useState({});
 
    const [loading, setLoading] = useState(true);
+
+   function genererAarListe(fraAaar, tilAar) {
+      const aarListe = [];
+    
+      for (let aar = fraAaar; aar <= tilAar; aar++) {
+        aarListe.push(aar);
+      }
+      
+      return aarListe.join(';');
+   }
+
+   function getAarArray(fraAaar, tilAar) {
+    const aarArray = [];
+  
+    for (let aar = fraAaar; aar <= tilAar; aar++) {
+      aarArray.push(aar);
+    }
+  
+    return aarArray;
+  }
 
     useEffect(() => {
           fetchData(config.zInvestBackendUrl + 'search/hentLeiligheter')
@@ -25,29 +46,32 @@ export function Inntektregnskap() {
             });
            
 
-          if (leilighetId != null && dato != null) {
-            const aar = dato.getFullYear();
+          if (leilighetId != null && dato != null && tilDato != null) {
+            const aar = genererAarListe(dato.getFullYear(), tilDato.getFullYear());
+            const url = "search/hentInntektRegnskapForFlereAar";
+
             const formData = { leilighetId, aar };
+            
             setLoading(true);
 
-            fetchData(config.zInvestBackendUrl + 'search/hentInntektRegnskap' + getUrlWithParamData(formData))
+            fetchData(config.zInvestBackendUrl + url + getUrlWithParamData(formData))
             .then(res => res)
               .then(data => {
                   setInntektData(data);
                   setLoading(false);          
               });  
           }
-    }, [leilighetId, dato]);
+    }, [leilighetId, dato, tilDato]);
 
     const handleLeilighetIdChange = event => {
             setleilighetId(event.target.value);
         };
 
 
-    const columns = [
-      { key: 'label', name: 'Label', minWidth: '20%' },
-      { key: 'belop', name: 'Beløp', minWidth: '80%' },
-    ];
+    // const columns = [
+    //   { key: 'label', name: 'Label', minWidth: '20%' },
+    //   { key: 'belop', name: 'Beløp', minWidth: '80%' },
+    // ];
 
     if (loading) {
         return <div>Loading...</div>;
@@ -63,12 +87,12 @@ export function Inntektregnskap() {
         <h1>Inntekt regnskap</h1>
         <h3>Leilighet</h3>
                         <select value={leilighetId} onChange={handleLeilighetIdChange}>
-                              <option key="">Velg leilighet</option>
+                              <option key="-1">Velg leilighet</option>
                               {leilighetRows.map(row => (
                                 <option key={row.id} value={row.id}>{row.navn}</option>
                               ))}
                         </select>
-        <h3>År</h3>
+        <h3>Fra år</h3>
                 <DatePicker
                      selected={dato}
                      onChange={dato => setDato(dato)}
@@ -76,21 +100,48 @@ export function Inntektregnskap() {
                      showYearPicker
                 />
 
+          <h3>Til år</h3>
+          <DatePicker
+               selected={tilDato}
+               onChange={dato => setTilDato(dato)}
+               dateFormat="yyyy"
+               showYearPicker
+          />
+
+
         {inntektData.length > 0 ? (
 
-            <table className='striped-table'>
-                 <tbody>
-                    {inntektData.map((item) => (
-                        <tr className={lineUnder(item.label)} key={item.id}>
-                          <td>{item.label}</td>
-                          <td>{item.belop}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-         ) : (
-             <p/>
-         )}
+          <table className='striped-table'>
+              <tbody>
+                <tr>
+                  <td/>
+                  {inntektData.map((item) => (
+                      item.aar !== null 
+                      ? <td><b>{item.aar}</b></td> 
+                      : ""                
+                  ))}                  
+                </tr>
+                  
+                {inntektData.map((item, index) => (
+                  item.aar === null ? (
+                      <tr className={lineUnder(item.label)} key={item.id}>
+                          {item.label !== "" && (
+                              <>
+                                <td key={index}>{item.label}</td>
+                                {item.belopList.map((belop, index2) => (
+                                  <td key={index2}>{belop}</td>
+                                ))}
+                              </>
+                          )}
+                      </tr>
+                  ) : ""
+                  ))}
+              </tbody>
+          </table>
+        ) : (
+        <p/>
+        )} 
+
 
         </>
       );
